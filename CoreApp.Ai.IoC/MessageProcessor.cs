f39ee94c0d7534359300360acc196295c29fb943
Mariia Kulidzhanova <mkulidzhanova@axmor.com>
2025-01-01 16:03:20 +0300

    Add message processor

---
+++ CoreApp.Ai.IoC/MessageProcessor.cs
@@ -0,0 +1,12 @@
+﻿namespace CoreApp.Ai.IoC
+{
+    public class MessageProcessor
+    {
+        public void ProcessMessage(UObject message)
+        {
+            ICommandHandler handler = IoC.Resolve(message.Action);
+            handler.Handle(message);
+        }
+    }
+
+}
