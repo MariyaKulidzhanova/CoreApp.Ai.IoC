7d39298aec3d712d09056f94125443857c688549
Mariia Kulidzhanova <mkulidzhanova@axmor.com>
2025-01-01 16:07:33 +0300

    Add console app to show example of handlers' usage

---
+++ CoreApp.Ai.IoC.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using CoreApp.Ai.IoC;
+using CoreApp.Ai.IoC.Models;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IoC.Initialize();
+
+        var messageProcessor = new MessageProcessor();
+
+        var startMoveMessage = new UObject { Action = "StartMove" };
+        startMoveMessage.SetVelocity(10);
+
+        var stopMoveMessage = new UObject { Action = "StopMove" };
+
+        messageProcessor.ProcessMessage(startMoveMessage);
+        messageProcessor.ProcessMessage(stopMoveMessage);
+    }
+}
