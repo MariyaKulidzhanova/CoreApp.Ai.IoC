f0a325f693b48853896dd7c3afa258ed9bc82495
Mariia Kulidzhanova <mkulidzhanova@axmor.com>
2025-01-01 15:58:11 +0300

    Use Autofac to resolve dependencies

---
+++ CoreApp.Ai.IoC/IoC.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+
+namespace CoreApp.Ai.IoC
+{
+    public static class IoC
+    {
+        private static IContainer _container;
+
+        public static void Initialize()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<StartMoveHandler>().Named<ICommandHandler>("StartMove");
+            builder.RegisterType<StopMoveHandler>().Named<ICommandHandler>("StopMove");
+            builder.RegisterType<StartRotationHandler>().Named<ICommandHandler>("StartRotation");
+            builder.RegisterType<StopRotationHandler>().Named<ICommandHandler>("StopRotation");
+
+            _container = builder.Build();
+        }
+
+        public static ICommandHandler Resolve(string command)
+        {
+            return _container.ResolveNamed<ICommandHandler>(command);
+        }
+    }
+}
